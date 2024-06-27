10edaab754af996b541090e93af02cca7199fced
Luis <lic_luiscastro@hotmail.com>
2024-06-26 19:57:05 -0600

    se trabaja con busquedas de las alcaldias 2

--- recomendations.py
+++ recomendations.py
@@ -5,7 +5,7 @@ import os
 import google.generativeai as genai
 import json
 
-def post_gemini(json_result_properties: dict):
+def post_gemini(json_result_properties: dict, perfil, purpose, price, bedrooms, parking_num):
     genai.configure(api_key="AIzaSyBsNf6f-iGUKqr8gJyJQ5BtmyOZgn7adsE")
 
     # Create the model
@@ -26,23 +26,17 @@ def post_gemini(json_result_properties: dict):
     output = """[{id: str,
             reason: str,
             price: int,
-            scoreTotal: float}]"""
-
-    perfil = "estudiante"
-    transaction = "venta"
-    price = 850000
-    habitaciones = 4
-    parking_num = 1
+            score: float}]"""
 
     prompt = f"""
-        Eres una persona con un perfil de {perfil}, un inmueble en {transaction}, con un presupuesto de {price} pesos MXN y con una elasticidad de 5%. 
+        Eres una persona con un perfil de {perfil}, un inmueble en {purpose}, con un presupuesto de {price} pesos MXN y con una elasticidad de 5%. 
         Tienes que encontrar los inmuebles ideales para este perfil, teniendo en cuenta lo siguiente:
-        El inmueble tiene que contar con al menos {habitaciones} recámaras y con {parking_num} estacionamientos.
+        El inmueble tiene que contar con al menos {bedrooms} recámaras y con {parking_num} estacionamientos.
         La persona quiere un inmueble cerca a Universidad Insurgentes Plantel Viaducto, Universidad Autónoma de la Ciudad de México Plantel Centro Histórico, Panteón Francés de la Piedad, Museo del Juguete Antiguo México, Mercado Jamaica.
 
         Ten en cuenta lo siguientes para los campos del JSON para la selección de las mejores propiedades:
         - price: Presupuesto de la persona.
-        - scoreTotal: Puntaje total de la propiedad del 1 al 100, teniendo en cuenta que hay unas propiedades que sobrepasan el 100 por bonificaciones adicionales.
+        - score: Puntaje total de la propiedad del 1 al 100, teniendo en cuenta que hay unas propiedades que sobrepasan el 100 por bonificaciones adicionales.
         - description: Descripción general del inmueble.
         - purpose: Propósito
         - sepomex_id: Alcaldía de CDMX
@@ -76,7 +70,7 @@ def post_gemini(json_result_properties: dict):
         ]
     )
 
-    print("||||||||||||||||||||||||||||")
+    # print("||||||||||||||||||||||||||||")
 
     response = chat_session.send_message("Dame la respuesta")
 
@@ -84,7 +78,7 @@ def post_gemini(json_result_properties: dict):
     json_response = json.loads(response.text)
     return json_response
 
-def post_recomendations():
+def post_user_preferences():
    
     # user_lifetime, purpose, max_price, city_id, places_interest, bedrooms, parking_num
     user_lifetime = "estudiante" 
@@ -130,27 +124,17 @@ def post_recomendations():
     row_insert = mycursor_w.rowcount
     print("-------- Se guarda informacion de quiz")
 
-    
-
-    # profiled_user= []
-    # for x in myresult:
-    #     profiled_user.append(x)
-    # result_profiled = pd.DataFrame(profiled_user, columns=['id','sepomex_id', 'purpose', 'type_children'])
-    
-    # print(result_profiled)
-    # print(f"RESULTADO")
-    # print(result_profiled['sepomex_id'])
-
-    # Propiedades similares
-    # print(f"Total: {len(result_profiled)}")
     json_result_properties = {}
     if  row_insert > 0:
         print("-------- Perfila propiedades de su interes")
         purpose_str = ""
+        purpose_value = ""
         if purpose == 1:
             purpose_str = "sale_price"
+            purpose_value = "venta"
         else:
             purpose_str = "rental_price"
+            purpose_value = "renta"
 
         query = f"""
             SELECT property_id, score, description, sepomex_id, purpose, type_children, {purpose_str} as price, bathrooms, bedrooms, parking_num
@@ -159,10 +143,7 @@ def post_recomendations():
             AND purpose = {purpose}
             AND type_children in (1,2)
             AND status = 1
-            -- AND {purpose_str} >= 1000000
-            -- AND {purpose_str} <= 1050000
             AND bedrooms >= {bedrooms}
-            -- AND parking_num >= 1
             ORDER BY score DESC
             limit 50
         """
@@ -182,7 +163,7 @@ def post_recomendations():
 
         # Envia datos a Gemini
         print("-------- Envia datos a Gemini")
-        recomendetions = post_gemini(json_result_properties)
+        recomendetions = post_gemini(json_result_properties, user_lifetime, purpose_value, max_price, bedrooms, parking_num)
         get_recomendations(recomendetions, conexion_w)
 
 def get_recomendations(recomendetions, conexion_w):
@@ -196,19 +177,19 @@ def get_recomendations(recomendetions, conexion_w):
     listToStrIds = ','.join([str(elem) for elem in property_list])
     
     query = f"""
-        SELECT property_id, score, description, sepomex_id, purpose, type_children, score, IF(purpose = 1, sale_price, rental_price) as price, bathrooms, bedrooms, parking_num
+        SELECT property_id, score, description, sepomex_id, purpose, type_children, IF(purpose = 1, sale_price, rental_price) as price, bathrooms, bedrooms, parking_num
         FROM properties_search
         WHERE property_id in ({listToStrIds})
     """
-    # print(query)
+    
     mycursor_w = conexion_w.cursor()
     mycursor_w.execute(query)
     myresult = mycursor_w.fetchall()
     properties = []
     for x in myresult:
         properties.append(x)
-    result_properties = pd.DataFrame(properties,columns=['id', 'score', 'description', 'sepomex_id', 'purpose', 'type_children', 'price', 'bathrooms', 'bedrooms','parking_num'])
+    result_properties = pd.DataFrame(properties,columns=['property_id', 'score', 'description', 'sepomex_id', 'purpose', 'type_children', 'price', 'bathrooms', 'bedrooms','parking_num'])
     
     print(result_properties)
 
-h = post_recomendations()
+h = post_user_preferences()
